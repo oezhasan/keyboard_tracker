e0961ff5eb740344440b020bcee79256de4832b5
oez-hasan <hasan.oez@live.de>
2021-09-01 21:59:12 +0200

    added SystemTrayIcon started to create window to display key + number of strokes

--- main.py
+++ main.py
@@ -1,7 +1,51 @@
+from PyQt5 import QtWidgets, QtGui
+from PyQt5.QtWidgets import QSystemTrayIcon, QMainWindow, QVBoxLayout, QLabel, QPushButton
+from PySide2.QtWidgets import QApplication, QWidget
 from pynput import keyboard
-import pickle
-import csv
+import os
+import sys
+from PyQt5 import QtGui, QtCore
 import sqlite3
+
+class SystemTrayIcon(QtWidgets.QSystemTrayIcon):
+    def __init__(self, icon, parent=None):
+        QtWidgets.QSystemTrayIcon.__init__(self,icon,parent)
+        self.setToolTip("Keyboard Tracker")
+        menu = QtWidgets.QMenu(parent)
+
+        self.setContextMenu(menu)
+        self.activated.connect(self.onTrayIconActivated)
+
+    def onTrayIconActivated(self, reason):
+        if reason == self.DoubleClick:
+            self.window = Keyboard_Tracker_Window()
+            self.window.show()
+
+
+class Keyboard_Tracker_Window(QMainWindow):
+    def __init__(self, parent=None):
+        super(Keyboard_Tracker_Window, self).__init__(parent)
+        self.resize(1200,800)
+        self.setWindowTitle("KEYBOARD TRACKER")
+
+        widget = QWidget()
+        layout = QVBoxLayout()
+
+        db_list = get_db_list()
+        #for list in db_list:
+        #    label = QLabel(f"Button: {list[0]}, Counter: {list[1]}")
+        #    layout.addWidget(label)
+
+        label = QLabel("HALLLLO")
+        layout.addWidget(label)
+        button1 = QPushButton("Button 1")
+        layout.addWidget(button1)
+        widget.setLayout(layout)
+
+        self.setCentralWidget(widget)
+
+
+
 
 
 def db(command):
@@ -38,6 +82,11 @@ def on_release(key):
 def keyboard_representation(k):
     pass
 
+def get_db_list():
+    conn = sqlite3.connect('keyboard_counter.db')
+    c = conn.cursor()
+    c.execute("SELECT * FROM keyboard_counter")
+    return c.fetchall()
 
 
 def track_key(key):
@@ -57,8 +106,18 @@ def track_key(key):
 
 
 if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    tray_icon = SystemTrayIcon(QtGui.QIcon("icon.ico"))
+    tray_icon.show()
+    app.exec_()
+
     #db("clear") #create, clear
-    key_listener()
+    #key_listener()
+
+
+
+
+
 
 
 
